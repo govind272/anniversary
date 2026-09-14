84874e5d11ec7bf32a9f9179cebe0c01f4816665
Govind Kumar <govind.it.24032@recb.ac.in>
2026-09-15 00:01:57 +0530

    Add personal details configuration
    Added configuration for personal details including names, password, anniversary date, and a final message.

---
+++ config.js
@@ -0,0 +1,25 @@
+const CONFIG = {
+
+    // =========================
+    // PERSONAL DETAILS
+    // =========================
+
+    girlfriendName: "PICCOLO",
+    boyfriendName: "PENGUIN",
+
+    // Password
+    password: "Priyansh1129",
+
+    // Anniversary
+    anniversaryDate: "14/09/2025",
+
+    // Final Message
+    finalMessage: `
+        Happy Anniversary ❤️
+
+        And this isn't the end...
+
+        It's just another chapter
+        of our story together.
+    `
+};
